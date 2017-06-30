1708efc8e5b91c3c49ef94904e9dfa76610d3134
natansevero <natanseverolp@gmail.com>
2017-06-30 15:16:17 -0300

    Configurando webpack e webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+// Configurar para ler um arquivo (Arquivo de desenvolvimento) e gerar o BUNDLE (Gerar pacote)
+'use strict';
+
+const path = require('path');
+
+// Exportando objeto que o webpack precisa para fazer a leitura do arquivo e gerar o bundle
+/*
+  entry: Caminho do arquivo principal que será lido
+  output: O webpack irá ler o arquivo do 'entry' e irá salavr onde tiver o output configurado
+    publicPath: É o local onde o webpack-dev-server irá fica assistindo nosso arquivo de desenvolvimento.
+                Quando o dev-server é executado, ele gera um arquivo virtual, em memoria e o publicPath
+                diz onde esse arquivo irá ficar
+*/
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  }
+}
